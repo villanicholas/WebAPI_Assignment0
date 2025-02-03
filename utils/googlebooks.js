23086a319e3e202878f8166b62b0ac753c8cba4b
villanicholas <92879090+villanicholas@users.noreply.github.com>
2025-02-02 19:06:08 -0700

    Update googlebooks.js

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -12,13 +12,13 @@ module.exports = async (phrase) => {
     //
     //TODO: return a formattedObj
     var formattedObj = {
-        data: results.data,
-        status: results.status,
-        statusText: results.statusText,
-        headers: results.headers,
-        requestHeader: results.config.headers
+        data: response.data,
+        status: response.status,
+        statusText: response.statusText,
+        headers: response.headers,
+        requestHeader: response.config.headers
     }
 
     //TODO: return the formattedObject
-    return JSON.stringify(results.data);
+    return JSON.stringify(response.data);
 }
